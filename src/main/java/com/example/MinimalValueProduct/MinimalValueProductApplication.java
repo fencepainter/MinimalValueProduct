ff1cab83be66b865d9522cc6089553b47ef0ccbb
fencepainter <fencepainter2@gmail.com>
2023-05-09 11:28:35 +0200

    First commit, fresh Spring Project

---
+++ src/main/java/com/example/MinimalValueProduct/MinimalValueProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.MinimalValueProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalValueProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalValueProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MinimalValueProduct/MinimalValueProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MinimalValueProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalValueProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
