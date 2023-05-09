52f14b52b260667312180bfb77c2e0355b16032b
fencepainter <fencepainter2@gmail.com>
2023-05-09 13:09:56 +0200

    Not found exception 404 added

---
+++ src/main/java/com/example/MinimalValueProduct/Main.java
@@ -0,0 +1,19 @@
+package com.example.MinimalValueProduct;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+
+
+}
