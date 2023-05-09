83ddad34eca3a56124740f7ac929cd9c370945e9
fencepainter <fencepainter2@gmail.com>
2023-05-09 12:41:14 +0200

    created Customer: Controller, Dao, Service

---
+++ src/main/java/com/example/MinimalValueProduct/Customer/CustomerController.java
@@ -0,0 +1,4 @@
+package com.example.MinimalValueProduct.Customer;
+
+public class CustomerController {
+}
---
+++ src/main/java/com/example/MinimalValueProduct/Customer/CustomerDataAccessService.java
@@ -0,0 +1,46 @@
+package com.example.MinimalValueProduct.Customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerDataAccessService implements CustomerDao{
+    // db
+    private static final List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+
+        Customer alex = new Customer(
+                1,
+                "Alex",
+                "alex@gmail.com",
+                true, false
+        );
+        customers.add(alex);
+
+        Customer jamila = new Customer(
+                2,
+                "Jamila",
+                "jamila@gmail.com",
+                true, false
+        );
+        customers.add(jamila);
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return customers.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst();
+    }
+}
+
